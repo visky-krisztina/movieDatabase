3c60abb8cf3772e63e7def5e4c3fa095bdf8f54f
visky-krisztina <127683091+visky-krisztina@users.noreply.github.com>
2024-06-07 18:41:09 +0300

    edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			"@components": path.resolve(__dirname, "./src/components"),
+		},
+		extensions: [".js", ".jsx"],
+	},
 });
